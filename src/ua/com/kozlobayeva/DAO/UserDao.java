0399b4691bd17d0a6f92bc74a7678ea7d749f013
Alina <alina4216@gmail.com>
2016-03-29 13:08:35 +0200

    Commit DAO implementation, welcomePage.jsp

--- src/ua/com/kozlobayeva/DAO/UserDao.java
+++ src/ua/com/kozlobayeva/DAO/UserDao.java
@@ -1,7 +1,30 @@
 package ua.com.kozlobayeva.DAO;
 
+import ua.com.kozlobayeva.Entity.User;
+
+
+import java.util.List;
+
 /**
- * Created by ASUS1 on 24.03.2016.
+  * Created by Alina on 24.03.2016.
  */
-public interface UserDao extends GenericDao {
+public interface UserDao extends GenericDao<String, User> {
+    /**
+     * Returns user with specific login.
+     * @param login specific key for finding user with this key
+     * @return User with login
+     */
+    User findUserByLogin(String login);
+
+    @Override
+    List<User> findAll();
+
+    @Override
+    boolean create(User user);
+
+    @Override
+    boolean update(User user);
+
+    @Override
+    boolean delete(String user);
 }
